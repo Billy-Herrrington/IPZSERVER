0595d99141d05d192b03b64e7b7d8429e8f32ab5
JenyaKrasulin <71638309+JenyaKrasulin@users.noreply.github.com>
2021-05-25 15:16:53 +0300

    1

---
+++ src/test/java/com/ddanilyuk/userDemo1/DeadlinesServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddanilyuk.userDemo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeadlinesServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
